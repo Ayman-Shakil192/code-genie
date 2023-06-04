1efd65364bbc7ff2d15866030cc47859a670274c
Ayman-Shakil192 <aymanshakilbms@gmail.com>
2023-06-05 01:10:06 +0530

    updated constants

---
+++ src/constants/languageData.js
@@ -0,0 +1,47 @@
+const languageData = [
+  {
+    id: 1,
+    name: "C",
+    image: "https://img.icons8.com/color/48/000000/c-programming.png",
+    code: "c",
+  },
+  {
+    id: 2,
+    name: "C++",
+    image: "https://img.icons8.com/color/48/000000/c-plus-plus-logo.png",
+    code: "cpp",
+  },
+  {
+    id: 3,
+    name: "Ruby",
+    image:
+      "https://img.icons8.com/color/48/000000/ruby-programming-language.png",
+    code: "ruby",
+  },
+  {
+    id: 4,
+    name: "JavaScript",
+    image: "https://img.icons8.com/color/48/000000/javascript.png",
+    code: "css",
+  },
+  {
+    id: 5,
+    name: "Go",
+    image: "https://img.icons8.com/color/48/000000/golang.png",
+    code: "go",
+  },
+  {
+    id: 6,
+    name: "Java",
+    image: "https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png",
+    code: "java",
+  },
+  {
+    id: 7,
+    name: "Python",
+    image: "https://img.icons8.com/color/48/000000/python.png",
+    code: "python",
+  },
+];
+
+export default languageData;
