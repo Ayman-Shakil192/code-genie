af618169be53c1c52fc1332deec4c86b0168ac20
Ayman-Shakil192 <aymanshakilbms@gmail.com>
2023-06-05 01:06:31 +0530

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Landing from "./components/Landing/Landing";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Compiler from "./components/Compiler/Compiler";
+import History from "./components/History/History";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/compiler" element={<Compiler />} />
+          <Route path="/history" element={<History />} />
         </Routes>
       </Router>
     </div>
