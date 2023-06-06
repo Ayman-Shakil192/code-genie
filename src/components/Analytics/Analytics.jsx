d393b4406f8610f3e4835787ad4560ebffb79ddf
Ayman-Shakil192 <aymanshakilbms@gmail.com>
2023-06-06 23:48:54 +0530

    created line chart analytics

---
+++ src/components/Analytics/Analytics.jsx
@@ -0,0 +1,83 @@
+//import { useEffect, useState } from "react";
+import {
+  AreaChart,
+  Area,
+  //   BarChart,
+  //   Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import graphData from "../../constants/graphData";
+
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth, db } from "../Firebase/Firebase";
+// import { collection, getDocs } from "firebase/firestore";
+import SideBar from "../Sidebar/SideBar";
+
+const Analytics = () => {
+  //   const [data, setData] = useState([]);
+  //   const [user] = useAuthState(auth);
+
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       try {
+  //         const querySnapshot = await getDocs(
+  //           collection(db, "users", user?.uid, "queries")
+  //         );
+  //         const responseData = querySnapshot.docs.map((doc) => doc.data());
+  //         setData(responseData);
+  //       } catch (error) {
+  //         console.error("Error fetching data: ", error);
+  //       }
+  //     };
+
+  //     fetchData();
+  //   }, [user?.uid]);
+
+  return (
+    <SideBar>
+      <ResponsiveContainer width="100%" height={500}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={graphData}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="uv"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="pv"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </SideBar>
+  );
+};
+
+export default Analytics;
